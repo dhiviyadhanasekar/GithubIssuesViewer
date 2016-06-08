51bef9d00ac8ca901a66b39ba0a7b8582fa8fbe9
Dhiviya. D <dhiviyadhanasekar@gmail.com>
2016-06-07 21:14:54 -0700

    loading jsx files

--- src/javascript/Test.jsx
+++ src/javascript/Test.jsx
@@ -1,6 +1,6 @@
 var Test = module.exports = React.createClass({
     displayName: 'TestHelloReact',
     render: function(){
-        return <div>Test jsx fiakjnfkjwle loading...</div>
+        return <div>Test jsx fiakjnrfrfkjwle loading...</div>
     }
 })
