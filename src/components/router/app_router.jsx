178ebcebc1bce9a835afb7e4b21594db5a988d32
Dhiviya. D <dhiviyadhanasekar@gmail.com>
2016-06-25 15:00:40 -0700

    remove redumdatn stuff

--- src/components/router/app_router.jsx
+++ src/components/router/app_router.jsx
@@ -5,7 +5,6 @@ var IndexRoute = require('react-router').IndexRoute;
 var createHashHistory = require('history').createHashHistory;
 var useRouterHistory = require('react-router').useRouterHistory;
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false }) ;
-var browserHistory = require('react-router').browserHistory;
 
 var IssuesViewer = require('components/issues_viewer/issues_viewer');
 var PageNotFound = require('components/page_not_found');
